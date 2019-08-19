2aefcf3735873010e67a143d02b4fd4245b8cd42
Dennis Ngeno <dennsngeno7@gmail.com>
2019-08-19 14:55:04 +0300

    chore(pagination): Add pagination
                       - Adds pagination
    
                       [Finishes #88839]

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
 
 from authors.models import Authors
 from authors.serializers import AuthorsSerializer
+from books.paginations import StandardResultsSetPagination
 
 
 class AuthorsViewSet(ModelViewSet):
-    queryset = Authors.objects.all()
+    queryset = Authors.objects.get_queryset().order_by('id')
     serializer_class = AuthorsSerializer
+    pagination_class = StandardResultsSetPagination
