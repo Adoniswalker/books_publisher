2c35785cb9bbc26e67acddace243656ef60d1650
Dennis Ngeno <dennsngeno7@gmail.com>
2019-08-19 13:41:52 +0300

    feat(authors):  Creating authors
                    - Create authors
                    - Create models
    
                    [Finished #7487585]

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from authors.models import Authors
+from authors.serializers import AuthorsSerializer
+
+
+class AuthorsViewSet(ModelViewSet):
+    queryset = Authors.objects.all()
+    serializer_class = AuthorsSerializer
